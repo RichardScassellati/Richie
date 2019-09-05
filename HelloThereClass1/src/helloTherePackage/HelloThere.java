747b728cfd32a82f1bf2288b5dad5d5edbb74363
1923scasrich <1923scasrich@10.242.2.240>
2019-09-05 12:15:46 -0400

    This is just a test program.

---
+++ HelloThereClass1/src/helloTherePackage/HelloThere.java
@@ -0,0 +1,26 @@
+package helloTherePackage;
+
+//Project Name: Hello World
+//This is America
+//I am Heavy Weapons Guy, and this is my program.
+/* 
+ * These are comments as well.
+ * nope.avi
+ * */
+
+public class HelloThere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String NewLine = "KENOBI";
+		System.out.println("Hello there."); //prints the beginning of the Star Wars meme
+		System.out.println("GENERAL KENOBI"); //prints the rest of the Star Wars meme
+		System.out.print("These are our top sellers:\n");
+		System.out.print("\tComputer games\n\tCoffee\n ");
+		System.out.println("\tAspirin");
+		System.out.print("GENERAL " + NewLine + " " + 9000 + "\nThis is a bucket.");
+		
+
+	}
+
+}
